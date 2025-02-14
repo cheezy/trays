236946421f358175311dc5ebf4edddf10daf78f0
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-02-14 10:57:12 -0500

    Added first test to make sure everything is configured.

---
+++ test/trays_web/live/trays_live/index_test.exs
@@ -0,0 +1,9 @@
+defmodule TraysWeb.TraysLive.IndexTest do
+    use TraysWeb.ConnCase
+
+
+  test "should load the main trays page", %{conn: conn} do
+      conn = get(conn, "/en/trays")
+      assert html_response(conn, 200) =~ "Trays"
+  end
+ end
