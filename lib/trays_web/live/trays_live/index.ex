494916257878eec45c5ecb984aa94f4678f5381b
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-02-14 09:37:28 -0500

    Localized first page and fixed french and german gettext directories

--- lib/trays_web/live/trays_live/index.ex
+++ lib/trays_web/live/trays_live/index.ex
@@ -4,13 +4,15 @@ defmodule TraysWeb.TraysLive.Index do
   @moduledoc false
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :page_title, "Trays")
+    socket = assign(socket, :page_title, gettext("Trays"))
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    {@page_title}
+    <div class="trays-index">
+      <h1>{@page_title}</h1>
+    </div>
     """
   end
 end
