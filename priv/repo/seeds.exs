f0213a712b5f7a7b7f88f7fd8d0a9640e63f0aef
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-02-17 08:44:22 -0500

    Added a few more merchants to my seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,7 +16,7 @@ alias Trays.Merchant
 
 %Merchant{
   name: "Au Pain Doré",
-  description: "Amazing Bakery and Café",
+  description: "Bakery location offering fresh bread, pastries & other sweets, plus a cafe with sandwiches & coffee.",
   contact_name: "Ardita Karaj",
   contact_phone: "4162304519",
   contact_email: "ardita@letstango.ca",
@@ -28,12 +28,36 @@ alias Trays.Merchant
 
 %Merchant{
   name: "Miku",
-  description: "Authentic Japanese restaurant",
-  contact_name: "Joseph Morgan",
+  description: "Flame-seared sushi is the specialty at this Japanese fine-dining destination with soaring ceilings.",
+  contact_name: "Hinako Abukawa",
   contact_phone: "4165551234",
   contact_email: "ko@miku.ca",
   logo_path: "/images/miku_logo.jpg",
-  food_category: "Japanese",
+  food_category: "Authentic Japanese restaurant",
   store_image_path: "/images/miku_store.jpg"
+}
+|> Repo.insert
+
+%Merchant{
+  name: "The Flat Iron",
+  description: "Classic British pub offering international brews & grub in a historic building with a patio.",
+  contact_name: "Joseph Morgan",
+  contact_phone: "6471234567",
+  contact_email: "joseph@gooddrinks.ca",
+  logo_path: "/images/flatiron_logo.png",
+  food_category: "British Pub",
+  store_image_path: "/images/flatiron_store.jpg"
+}
+|> Repo.insert
+
+%Merchant{
+  name: "The Sultan's Tent",
+  description: "French-Moroccan fare is served in the front cafe, with belly dancers & plush divans in a back room.",
+  contact_name: "Livia Whynott",
+  contact_phone: "6479993487",
+  contact_email: "libia@sultantstent.ca",
+  logo_path: "/images/sultans_tent_logo.png",
+  food_category: "Mediterranean restaurant",
+  store_image_path: "/images/sultans_tent_store.jpg"
 }
 |> Repo.insert
