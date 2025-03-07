b232b1c4cd4961a87f60804fdfb0140d70fa8761
j-morgan6 <jsm10242000@gmail.com>
2025-03-07 15:20:06 -0500

    fixed merchant location tests

--- test/trays/trays/merchant_location_test.exs
+++ test/trays/trays/merchant_location_test.exs
@@ -23,33 +23,51 @@ defmodule Trays.MerchantLocationTest do
   end
 
   test "should require at least 3 characters for a street name", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(3))
+    changeset = changeset_with(context.valid_attributes, :street1, string_of_length(3))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :street1, string_of_length(2))
+    |> assert_validation_error_on(:street1, "should be at least 3 character(s)")
   end
 
   test "should not allow a street name to have more than 100 characters", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(101))
+    changeset = changeset_with(context.valid_attributes, :street, string_of_length(100))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :street1, string_of_length(101))
+    |> assert_validation_error_on(:street1, "should be at most 100 character(s)")
   end
 
   test "should require a city to have at least 3 characters", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(3))
+    changeset = changeset_with(context.valid_attributes, :city, string_of_length(3))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :city, string_of_length(2))
+    |> assert_validation_error_on(:city, "should be at least 3 character(s)")
   end
 
   test "should not allow a city to have more than 100 characters", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(4))
+    changeset = changeset_with(context.valid_attributes, :city, string_of_length(100))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :city, string_of_length(101))
+    |> assert_validation_error_on(:city, "should be at most 100 character(s)")
   end
 
   test "should require a province to have at least 2 characters", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(4))
+    changeset = changeset_with(context.valid_attributes, :province, string_of_length(2))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :province, string_of_length(1))
+    |> assert_validation_error_on(:province, "should be at least 2 character(s)")
   end
 
   test "should not allow a province to have more than 30 characters", context do
-    changeset = changeset_with(context.valid_attributes, :name, string_of_length(4))
+    changeset = changeset_with(context.valid_attributes, :province, string_of_length(30))
     assert changeset.valid? == true
+
+    changeset_with(context.valid_attributes, :province, string_of_length(31))
+    |> assert_validation_error_on(:province, "should be at most 30 character(s)")
   end
 
   defp assert_require_field(valid_attrs, field) do
