7a6596eb33a9f52b74f569dac70845d8b135a38c
j-morgan6 <jsm10242000@gmail.com>
2025-03-26 14:14:54 -0400

    tested editing hours of delivery for merchant location, edited merchant location appearance

--- test/trays_web/live/admin/merchant_location_live/form_test.exs
+++ test/trays_web/live/admin/merchant_location_live/form_test.exs
@@ -28,7 +28,8 @@ defmodule TraysWeb.Admin.MerchantLocationLive.FormTest do
 
       view
       |> form("#merchant-location-form")
-      |> render_submit(%{"merchant_location" => %{
+      |> render_submit(%{
+        "merchant_location" => %{
           "street1" => "Street One",
           "street2" => "Street Two",
           "city" => "Toronto",
@@ -55,9 +56,9 @@ defmodule TraysWeb.Admin.MerchantLocationLive.FormTest do
       {:ok, view, _html} = live(conn, "/en/admin/merchants/#{merchant.id}/locations/new")
 
       assert view
-      |> element("#merchant-location-form")
-      |> render_change(MerchantLocationFixtures.valid_merchant_location_attrs(%{city: ""}))
-          =~ @cant_be_blank_error
+             |> element("#merchant-location-form")
+             |> render_change(MerchantLocationFixtures.valid_merchant_location_attrs(%{city: ""})) =~
+               @cant_be_blank_error
     end
   end
 
@@ -65,36 +66,66 @@ defmodule TraysWeb.Admin.MerchantLocationLive.FormTest do
     setup %{conn: conn} do
       user = AccountsFixtures.user_fixture()
       merchant = MerchantFixtures.merchant_fixture_with_user(user)
-      location = MerchantLocationFixtures.merchant_location_fixture(merchant, %{contact_id: user.id})
+
+      location =
+        MerchantLocationFixtures.merchant_location_fixture(merchant, %{contact_id: user.id})
 
       %{conn: log_in_user(conn, user), user: user, merchant: merchant, location: location}
     end
 
-    test "should load the merchant location edit page", %{conn: conn, merchant: merchant, location: location} do
+    test "should load the merchant location edit page", %{
+      conn: conn,
+      merchant: merchant,
+      location: location
+    } do
       conn = get(conn, ~p"/en/admin/merchants/#{merchant.id}/locations/#{location.id}/edit")
       assert html_response(conn, 200) =~ "Edit Merchant Location"
     end
 
-    test "should edit a merchant location", %{conn: conn, merchant: merchant, location: location, user: user} do
-      {:ok, view, _html} = live(conn, "/en/admin/merchants/#{merchant.id}/locations/#{location.id}/edit")
+    test "should edit a merchant location", %{
+      conn: conn,
+      merchant: merchant,
+      location: location,
+      user: user
+    } do
+      {:ok, view, _html} =
+        live(conn, "/en/admin/merchants/#{merchant.id}/locations/#{location.id}/edit")
 
       view
       |> form("#merchant-location-form")
-      |> render_submit(%{"merchant_location" => %{
-        "street1" => "Street Three",
-        "street2" => "Street Four",
-        "city" => "London",
-        "province" => "ON",
-        "postal_code" => "M1M 1M2",
-        "country" => "Canada",
-        "delivery_option" => "both",
-        "prep_time" => 48,
-        "cancellation_policy" => "edit cancellation policy",
-        "special_instruct" => "new instruction to get to location"
-      }
+      |> render_submit(%{
+        "merchant_location" => %{
+          "street1" => "Street Three",
+          "street2" => "Street Four",
+          "city" => "London",
+          "province" => "ON",
+          "postal_code" => "M1M 1M2",
+          "country" => "Canada",
+          "delivery_option" => "both",
+          "prep_time" => 48,
+          "cancellation_policy" => "edit cancellation policy",
+          "special_instruct" => "new instruction to get to location",
+          "hours_of_delivery[monday][start_time]" => "11:00:00",
+          "hours_of_delivery[monday][end_time]" => "19:00:00",
+          "hours_of_delivery[tuesday][start_time]" => "11:00:00",
+          "hours_of_delivery[tuesday][end_time]" => "19:00:00",
+          "hours_of_delivery[wednesday][start_time]" => "11:00:00",
+          "hours_of_delivery[wednesday][end_time]" => "19:00:00",
+          "hours_of_delivery[thursday][start_time]" => "11:00:00",
+          "hours_of_delivery[thursday][end_time]" => "19:00:00",
+          "hours_of_delivery[friday][start_time]" => "11:00:00",
+          "hours_of_delivery[friday][end_time]" => "19:00:00",
+          "hours_of_delivery[saturday][start_time]" => "11:00:00",
+          "hours_of_delivery[saturday][end_time]" => "19:00:00",
+          "hours_of_delivery[sunday][start_time]" => "11:00:00",
+          "hours_of_delivery[sunday][end_time]" => "19:00:00"
+        }
       })
 
-      [location | _] = MerchantLocation |> Repo.all() |> Repo.preload(:contact)
+      location =
+        Repo.get!(MerchantLocation, location.id)
+        |> Repo.preload([:contact, :hours_of_delivery])
+
       {path, flash} = assert_redirect(view)
       assert location.contact.id == user.id
       assert location.delivery_option == :both
